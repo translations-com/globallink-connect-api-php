261dd3f621e132b73d3c60d49ea04e94a9cbc668
Santosh Pai <pai@users.noreply.github.com>
2016-11-16 15:43:08 +0530

    Delete create-phar.php

--- create-phar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$srcRoot = dirname(__FILE__)."/GLExchange";
-$buildRoot = dirname(__FILE__);
- 
-$phar = new Phar($buildRoot . "/glexchange.phar", 
-	FilesystemIterator::CURRENT_AS_FILEINFO |     	FilesystemIterator::KEY_AS_FILENAME, "glexchange.phar");
-$phar->buildFromDirectory($srcRoot);
-
-?>
